9f6a5238f09650755cf1e4d96db3ca384ffe7f19
uday <udayahamindrakar4441@gmail.com>
2025-12-10 00:16:31 +0530

    navbar completed

--- frontend/airbnb/src/app/dashboard/dashboard.component.ts
+++ frontend/airbnb/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home/home.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, AppHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
