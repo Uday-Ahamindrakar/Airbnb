d99ee8d5e18f3631044a7deaa695eef1871d3b3c
uday <udayahamindrakar4441@gmail.com>
2025-12-09 16:51:32 +0530

    Initial project setup

---
+++ frontend/airbnb/src/app/dashboard/profile/host-profile/host-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './host-profile.component.html',
+  styleUrl: './host-profile.component.css'
+})
+export class HostProfileComponent {
+
+}
